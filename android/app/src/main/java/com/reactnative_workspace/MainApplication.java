8f5891149036b1d1d36f0c11af292e965c574bec
Babitdor Kayang Khonglah <57758789+Babitdor@users.noreply.github.com>
2022-11-29 22:02:32 +0530

    Add files via upload

--- android/app/src/main/java/com/reactnative_workspace/MainApplication.java
+++ android/app/src/main/java/com/reactnative_workspace/MainApplication.java
@@ -1,5 +1,5 @@
 package com.reactnative_workspace;
-
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
